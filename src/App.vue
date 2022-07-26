367eba6ed50a33c4d6e261e984ec8e7855480c49
ShrikantNale-angularminds <shrikant.nale@angularminds.in>
2022-07-26 14:22:21 +0530

    update

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,40 @@
   <GreetUser name="Clark" heroName="Superman" />
   <GreetUser name="Diana" heroName="Wonder woman" />
   <GreetUser :name="name" :heroName="channel" /> -->
-  <ArticleComp
+
+  <!-- <ArticleComp
     id="my-article"
     title="Article title"
     :likes="50"
     :isPublished="true"
-  />
+  /> -->
+
+  <h2>AppComponent</h2>
+  <h3>AppComponent username {{ name }}</h3>
+  <ComponentC />
 </template>
 
 <script>
 // import GreetUser from "./components/GreetUser.vue";
-import ArticleComp from "./components/ArticleComp.vue";
+// import ArticleComp from "./components/ArticleComp.vue";
+import ComponentC from "./components/ComponentC.vue";
 
 export default {
   name: "App",
   components: {
     // GreetUser,
-    ArticleComp,
+    // ArticleComp,
+    ComponentC,
   },
   data() {
     return {
       name: "Shrikant",
       channel: "Codevolution",
+    };
+  },
+  provide() {
+    return {
+      username: this.name,
     };
   },
 };
--- src/components/ComponentF.vue
+++ src/components/ComponentF.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <h2>Component F</h2>
+    <h3>Component F username {{ username }}</h3>
   </div>
 </template>
 
 <script>
 export default {
   name: "ComponentF",
+  inject: ["username"],
 };
 </script>
 
